e5e5e9438f4ed28810258d368eec61d05128bc4f
Shailendra Shukla <shailendrashukla1482@gmail.com>
2025-05-25 13:23:17 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,5 @@
 import sys
-import logging
-
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -17,8 +16,3 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
-    
-try:
-    a = 1 / 0
-except Exception as e:
-    raise CustomException(e, sys)
